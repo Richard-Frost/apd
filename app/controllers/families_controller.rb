6a00d24d7889ce5ce95e02591fdfeb05581ee99d
Richard-Frost <frost.rchrd@gmail.com>
2018-10-25 15:23:34 -0400

    added families_controller and playdates_controller, modified routes

---
+++ app/controllers/families_controller.rb
@@ -0,0 +1,19 @@
+class ChildrenController < ActionController::Base
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+end
+
---
+++ app/controllers/playdates_controller.rb
@@ -0,0 +1,18 @@
+class PlaydatesController < ActionController::Base
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+end
